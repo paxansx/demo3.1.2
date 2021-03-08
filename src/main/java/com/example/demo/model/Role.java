0a9b69e2a5012d17d0f223d81b67d65825f07fd0
paxansx <60510220+paxansx@users.noreply.github.com>
2021-03-08 21:01:53 +0300

    44

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -40,15 +40,6 @@ public class Role implements GrantedAuthority {
         this.role = role;
     }
 
-
-//    public Set<User> getUsers() {
-//        return users;
-//    }
-//
-//    public void setUsers(Set<User> users) {
-//        this.users = users;
-//    }
-
     @Override
     public String getAuthority() {
         return role;
